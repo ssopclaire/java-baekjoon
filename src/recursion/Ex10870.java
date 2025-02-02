0e65f9b6515b12883615c29230fe2609b44dc2f6
sy <ssopclaire@gmail.com>
2025-02-02 20:12:45 +0900

    Ex10870

---
+++ src/recursion/Ex10870.java
@@ -0,0 +1,26 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Ex10870 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
