dbace8507d9ae21c72a49672ad3e2bd35b17326a
sy <ssopclaire@gmail.com>
2025-02-07 15:21:01 +0900

    Ex2839

---
+++ src/bruteforce/Ex2839.java
@@ -0,0 +1,27 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2839 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            int count = 0;
+
+            while (n >= 0) {
+                if (n % 5 == 0) {
+                    count += n / 5;
+                    System.out.println(count);
+                    return;
+                }
+                n -= 3;
+                count++;
+            }
+
+            System.out.println(-1);
+        } catch (IOException e) {
+        }
+    }
+}
