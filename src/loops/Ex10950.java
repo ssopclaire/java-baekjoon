4dd1963c16d575eb2c7f51a40ae89d179812e62d
sy <ssopclaire@gmail.com>
2025-02-19 21:39:15 +0900

    Ex10950

---
+++ src/loops/Ex10950.java
@@ -0,0 +1,25 @@
+package loops;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex10950 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int t = Integer.parseInt(br.readLine());
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < t; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(a + b).append("\n");
+            }
+
+            System.out.print(sb);
+        } catch (IOException e) {
+        }
+    }
+}
