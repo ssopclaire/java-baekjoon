51673f1ba77eb9ed235d182a8cc3b4ee7481c1e1
sy <ssopclaire@gmail.com>
2025-02-28 18:58:30 +0900

    Ex2439

---
+++ src/loops/Ex2439.java
@@ -0,0 +1,23 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Ex2439 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
