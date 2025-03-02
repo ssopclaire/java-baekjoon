e1206051a9aa5f84355becf8c8b8a2ddd73f792d
sy <ssopclaire@gmail.com>
2025-03-02 11:05:12 +0900

    Ex10952

---
+++ src/loops/Ex10952.java
@@ -0,0 +1,27 @@
+package loops;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex10952 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            while (true) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                if (a == 0 && b == 0) break;
+
+                sb.append(a + b).append("\n");
+            }
+
+            System.out.println(sb);
+        } catch (IOException e) {
+        }
+    }
+}
