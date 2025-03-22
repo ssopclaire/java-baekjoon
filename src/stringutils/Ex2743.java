74f360b1ce2cbcfd78d1a513f473beea2954e41a
sy <ssopclaire@gmail.com>
2025-03-22 22:29:25 +0900

    Ex2743

---
+++ src/stringutils/Ex2743.java
@@ -0,0 +1,15 @@
+package stringutils;
+
+import java.util.Scanner;
+
+public class Ex2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+
+        System.out.println(word.length());
+
+        sc.close();
+    }
+}
