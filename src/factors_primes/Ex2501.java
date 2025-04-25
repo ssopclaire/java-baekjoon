da8f5eb3b3fb7183b8d282357dd7ee31c5d382cc
sy <ssopclaire@gmail.com>
2025-04-25 23:16:04 +0900

    Ex2501

---
+++ src/factors_primes/Ex2501.java
@@ -0,0 +1,29 @@
+package factors_primes;
+
+import java.util.Scanner;
+
+public class Ex2501 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int count = 0;
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (count == k) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
