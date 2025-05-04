8647cd1b5c7dc4be727fd9bd1feb027810cdfb28
sy <ssopclaire@gmail.com>
2025-05-04 22:00:04 +0900

    Ex1085

---
+++ src/geometry/Ex1085.java
@@ -0,0 +1,24 @@
+package geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex1085 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            int minDist = Math.min(Math.min(x, w - x), Math.min(y, h - y));
+
+            System.out.println(minDist);
+        } catch (IOException e) {
+        }
+    }
+}
