b81ec7c363b175ca6cfb3d4b2f57cc896e492f42
sy <ssopclaire@gmail.com>
2025-05-09 23:40:58 +0900

    Ex10101

---
+++ src/geometry/Ex10101.java
@@ -0,0 +1,27 @@
+package geometry;
+
+import java.util.Scanner;
+
+public class Ex10101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        sc.close();
+
+        int sum = a + b + c;
+
+        if (sum != 180) {
+            System.out.println("Error");
+        } else if (a == b && b == c) {
+            System.out.println("Equilateral");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
+        }
+    }
+}
