aee80aea01a04b45f99748ad25033484eba9b454
sy <ssopclaire@gmail.com>
2025-05-15 21:29:00 +0900

    Ex15894

---
+++ src/geometry/Ex15894.java
@@ -0,0 +1,13 @@
+package geometry;
+
+import java.util.Scanner;
+
+public class Ex15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        sc.close();
+
+        System.out.println(4 * n);
+    }
+}
