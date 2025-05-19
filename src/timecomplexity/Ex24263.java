84f0595703c858abc91eb92092ed79dc3bd5b710
sy <ssopclaire@gmail.com>
2025-05-19 21:27:04 +0900

    Ex24263

---
+++ src/timecomplexity/Ex24263.java
@@ -0,0 +1,14 @@
+package timecomplexity;
+
+import java.util.Scanner;
+
+public class Ex24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
