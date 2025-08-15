70daca697827fe7a783433399a03b77d361a6eeb
sy <ssopclaire@gmail.com>
2025-08-15 22:57:14 +0900

    Ex2579

---
+++ src/dynamic_programming1/Ex2579.java
@@ -0,0 +1,41 @@
+package dynamic_programming1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2579 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] score = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                score[i] = Integer.parseInt(br.readLine());
+            }
+
+            if (n == 1) {
+                System.out.println(score[1]);
+                return;
+            }
+
+            if (n == 2) {
+                System.out.println(score[1] + score[2]);
+                return;
+            }
+
+            int[] dp = new int[n + 1];
+
+            dp[1] = score[1];
+            dp[2] = score[1] + score[2];
+
+            for (int i = 3; i <= n; i++) {
+                dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+            }
+
+            System.out.println(dp[n]);
+        } catch (IOException e) {
+        }
+    }
+}
