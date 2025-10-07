97cd146ac4733c400efce114668a335995789e3e
sy <ssopclaire@gmail.com>
2025-10-07 20:47:34 +0900

    Ex1927

---
+++ src/priority_queue/Ex1927.java
@@ -0,0 +1,31 @@
+package priority_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Ex1927 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+
+            int n = Integer.parseInt(br.readLine());
+
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+            for (int i = 0; i < n; i++) {
+                int x = Integer.parseInt(br.readLine());
+
+                if (x == 0) {
+                    sb.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+                } else {
+                    pq.offer(x);
+                }
+            }
+
+            System.out.print(sb);
+        } catch (IOException e) {
+        }
+    }
+}
