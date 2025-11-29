9d5a99fc7d5ef36a88ebdccfb6676979c2b538e2
sy <ssopclaire@gmail.com>
2025-11-29 21:44:01 +0900

    Ex1008

---
+++ src/io_arithmetic/Ex1008.java
@@ -0,0 +1,16 @@
+package io_arithmetic;
+
+import java.util.Scanner;
+
+public class Ex1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+
+        System.out.println(a / b);
+
+        sc.close();
+    }
+}
