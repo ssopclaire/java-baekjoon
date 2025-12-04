b6a9e7c0ee4f125b4a11b872b156b38cf13a0a27
sy <ssopclaire@gmail.com>
2025-12-04 10:48:08 +0900

    Ex10430

---
+++ src/io_arithmetic/Ex10430.java
@@ -0,0 +1,28 @@
+package io_arithmetic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex10430 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.append((a + b) % c).append('\n');
+            sb.append(((a % c) + (b % c)) % c).append('\n');
+            sb.append((a * b) % c).append('\n');
+            sb.append(((a % c) * (b % c)) % c).append('\n');
+
+            System.out.print(sb);
+        } catch (IOException e) {
+        }
+    }
+}
