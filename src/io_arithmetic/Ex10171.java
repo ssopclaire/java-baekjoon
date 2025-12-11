57d2774dcc95b64b61ce9040796bd6f0723ba769
sy <ssopclaire@gmail.com>
2025-12-11 17:06:59 +0900

    Ex10171

---
+++ src/io_arithmetic/Ex10171.java
@@ -0,0 +1,10 @@
+package io_arithmetic;
+
+public class Ex10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
