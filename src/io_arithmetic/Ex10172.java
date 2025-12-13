8a6c3fdc762e3802f87d1ab48dbfae9ce58cf836
sy <ssopclaire@gmail.com>
2025-12-13 19:28:08 +0900

    Ex10172

---
+++ src/io_arithmetic/Ex10172.java
@@ -0,0 +1,11 @@
+package io_arithmetic;
+
+public class Ex10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
